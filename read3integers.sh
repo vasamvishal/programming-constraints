124f3667865293f3012f7900628f54900be2b4eb
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-11-11 18:13:20 +0530

    done usercase4

--- read3integers.sh
+++ read3integers.sh
@@ -3,17 +3,29 @@
  read -p "Enter first number:" a
  read -p "Enter second number:" b  
  read -p "Enter third number:" c 
+declare -A dictionaryy
+firstoperationresult=$(($a*$b+$c))
+echo "result of a*b+c " $firstoperationresult
+dictionaryy[counter1]=$firstoperationresult
 
-result=$(($a*$b+$c))
-echo "result of a+b*c " $result
 
-result=$(($a+$b*$c))
-echo "result of a*b+c " $result
+secondoperationresult=$(($a+$b*$c))
+echo "result of a+b*c " $secondoperationresult
+dictionaryy[counter2]=$secondoperationresult
 
-result=$(($c+$a/$b))
-echo "result of c+a/b " $result
 
-result=$(($a%$b+$c))
-echo "result of a%b+c " $result
+thirdoperationresult=$(($c+$a/$b))
+echo "result of c+a/b " $thirdoperationresult
+
+dictionaryy[counter3]=$thirdoperationresult
+
+
+fourthoperationresult=$(($a%$b+$c))
+echo "result of a%b+c " $fourthoperationresult
+dictionaryy[counter4]=$fourthoperationresult
+
+
+echo  ${dictionaryy[@]} value ${!dictionaryy[@]}
+
 
 
